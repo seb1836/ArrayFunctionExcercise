093ada890b33bb89251d9a553229cbc3b13c2d13
phung <seb111.penna@gmail.com>
2018-08-28 20:57:27 +0200

    first commit

---
+++ revision.js
@@ -0,0 +1,45 @@
+var hero = {
+	name:'toto',
+	hp: 450,
+}
+
+var addHP = function (hp,hero) {
+	hero.hp = addition(hp,hero.hp)
+
+	return hero.name "a" + hero.hp + "hp";
+	
+}
+
+
+var arrayMax = function(a) {
+	var c = 0;
+	let i = 0;
+	while (i < array.length) {
+		console.log(array[i]);
+		if (array[i]> c) {
+			console.log("value change",array[i]);
+			 c = array[i];
+
+		}
+
+			i = i+1;
+	}
+	return c;
+}	
+
+
+var arraysquare = function (a) {
+	let i = 0;
+	while (i < array.length) {
+		var b = array[i] * array[i];
+		b = array[i+1];
+		i = i+1;
+	}
+	return square;
+}
+
+var arrayFindIndex = function (a) {
+	let i = 0;
+	while (i < array.length) {
+
+}
